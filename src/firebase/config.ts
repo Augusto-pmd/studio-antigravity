7a780e6588774e7b63269908fb1ee0935ec02e3c
Augusto Menendez <info@pmdarquitectura.com>
2026-01-22 16:55:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8541008846-bde52",
+  "appId": "1:529763095747:web:20d2ec6a98490e78387e76",
+  "apiKey": "AIzaSyCFokWxQ-c6IOzqnA0-M11F_NVOxVPwhNw",
+  "authDomain": "studio-8541008846-bde52.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "529763095747"
+};
