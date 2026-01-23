e10cd990d48040e0eb202b01b549601c317f6cb7
Augusto Menendez <info@pmdarquitectura.com>
2026-01-23 15:18:02 +0000

    no aparece el asterisco, no se esta actualizando

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="" suppressHydrationWarning>
       <head>
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
