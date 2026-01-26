5c370f51f86852e434acee744ab0df98ad41a3ed
Augusto Menendez <info@pmdarquitectura.com>
2026-01-26 12:02:41 +0000

    Error Técnico Identificado: Se ha detectado un FirebaseError: Function s

--- src/components/tesoreria/add-treasury-account-dialog.tsx
+++ src/components/tesoreria/add-treasury-account-dialog.tsx
@@ -52,7 +52,7 @@ export function AddTreasuryAccountDialog({ children }: { children: React.ReactNo
           currency,
           accountType,
           balance: parseFloat(initialBalance) || 0,
-          cbu: accountType === 'Banco' ? cbu : undefined
+          ...(accountType === 'Banco' && { cbu: cbu })
       };
 
       setDoc(accountRef, newAccount, { merge: false })
