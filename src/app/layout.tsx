e80710f333afe88be4b8da7ef247cdf8af554077
Augusto Menendez <info@pmdarquitectura.com>
2026-02-13 15:38:18 +0000

    arregla el problema de visualizacion de la iterfaz que esta fallando

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased border-t-8 border-red-500">
-        <div className="w-full bg-yellow-300 text-black text-center font-bold p-4 text-2xl">SISTEMA ACTUALIZADO 2026</div>
+      <body className="font-body antialiased">
         <FirebaseClientProvider>
           <SidebarProvider>
             <AppShell>{children}</AppShell>
